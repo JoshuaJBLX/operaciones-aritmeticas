7be801ef0c118b042a5e40f9014819790bb8ee78
JoshuaBLX <73940475@continental.edu.pe>
2026-01-06 19:10:14 -0500

    Nuevo Modo

--- operaciones-aritmeticas.py
+++ operaciones-aritmeticas.py
@@ -1,6 +1,13 @@
-def s_d_n(n1,n2):
-    return n1+n2
+class OA:
+    def _init_(self,n1=0,n2=0):
+        self.n1=n1
+        self.n2=n2
+
+    def s_d_n(self):
+        return self.n1 + self.n2
 
 n1= int(input("Ingrese el primer Numero: "))
 n2= int(input("Ingrese el segundo Numero: "))
-print("la suma es: ", s_d_n(n1,n2))
+
+operaciones=OA(n1,n2)
+print("la suma es: ", operaciones.s_d_n())
